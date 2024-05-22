4092c394791f846d0826c77d75ee1e36227f33d5
apollo <apollo.bradshaw@gmail.com>
2024-05-22 17:14:16 +1000

    first test running, need to verify it's actually testing the thing

--- src/tests/multihost/ad/test_ldap_krb5.py
+++ src/tests/multihost/ad/test_ldap_krb5.py
@@ -1,4 +1,5 @@
 import pytest
+from sssd_test_framework.topology import KnownTopology
 from sssd_test_framework.roles.client import Client
 from sssd_test_framework.roles.generic import GenericProvider
 from sssd_test_framework.topology import KnownTopologyGroup
@@ -6,9 +7,8 @@ from sssd_passwd import (call_sssd_getpwnam)
 from sssd_nss import NssReturnCode
 
 @pytest.mark.ticket(bz=748833)
-@pytest.mark.topology(KnownTopology.LDAP)
 @pytest.mark.topology(KnownTopology.AD)
-def test_enumerate_and_authenticate_user01_over_STARTTLS(multihost, create_aduser_group):
+def test_enumerate_and_authenticate_user01_over_STARTTLS(client: Client, ad: AD):
     """
     :title: Enumerate and Authenticate user01 over STARTTLS bz748833
     :setup:
@@ -22,28 +22,22 @@ def test_enumerate_and_authenticate_user01_over_STARTTLS(multihost, create_aduse
         2. Authentication with password is successful
     :customerscenario: True
     """
-    (aduser, _) = create_aduser_group
-    client = sssdTools(multihost.client[0], multihost.ad[0])
-    dom_section = "XXX" # f'domain/{client.get_domain_section_name()}'
-    sssd_params = {
-        'ldap_uri': "ldap://"+multihost.ad[0].hostname,
+    ad.user('lkuser01').add()
+
+    client.sssd.config["domain/AD"] = {
+        'ldap_uri': "ldap://"+ad.host.hostname,
         'ldap_schema': 'ad',
         'debug_level': '0xFFF0',
         'description': 'LDAP domain with AD server',
         'id_provider': 'ldap',
         'ldap_id_use_start_tls': 'true',
-        'ldap_default_bind_dn': "cn=Administrator,cn=Users,"+multihost.ad[0].domainname,
+        'ldap_default_bind_dn': "cn=Administrator,cn=Users,"+ad.domain,
         'ldap_tls_cacert': '/etc/openldap/certs/ad_cert.pem',
-        'ldap_default_authtok': $AD_SERVER1_BINDPASS,
-        'ldap_search_base': multihost.ad[0].domainname,
+        'ldap_default_authtok': 'Secret123',
+        'ldap_search_base': ad.domain,
         'ldap_force_upper_case_realm': 'True',
         'ldap_referrals': 'false',
     }
-    client.sssd_conf(dom_section, sssd_params)
-    # Clear cache and restart SSSD
-    client.clear_sssd_cache()
-    res,_ = call_sssd_getpwnam(aduser)
-    assert res == NssReturnCode.SUCCESS
-    password_login_res = client.auth_from_client(aduser, 'Secret123')
-    assert password_login_res == 3 # this was used in sssd/src/tests/multihost/ad/test_adparameters_ported.py, need to check it's right
-
+    client.sssd.start()
+    assert client.tools.id('lkuser01') is not None
+    assert client.auth.ssh.password('lkuser01', 'Secret123')
